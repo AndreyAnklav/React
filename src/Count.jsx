b2190f0f1f84a2a207283ecaa8c0955018714baa
AndreyAnklav <142445234+AndreyAnklav@users.noreply.github.com>
2023-10-25 22:14:26 +0300

    Задание №3

---
+++ src/Count.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function Count() {
+    let [count, setCount] = useState(0);
+    
+function handleClick(){
+    setCount(count + 1)
+}
+function handleMinus(){
+    if(count !== 0){
+    setCount(count - 1)
+}   
+}
+function sbros(){
+    setCount(count = 0)
+}
+
+  return (
+    <div>
+      <p>Вы кликнули {count} раз</p>
+      <button onClick={handleClick}>Плюс</button>
+      <button onClick={handleMinus} >Минус</button>
+      <button onClick={sbros}>Сброс</button>
+    </div>
+  )
+}
